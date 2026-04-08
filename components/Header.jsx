ff1009dba4f17670c91918481bcff1161cfc3b32
Matthew Wolford <mg.wolford@outlook.com>
2026-04-08 14:05:01 -0400

    Updates files/images

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,9 @@
+import helmetImg from "../assets/helmet.png";
+
 function Header() {
   return (
     <header className="header">
-      <img src="../dist/assets/helmet.png" alt="Super Wacky Fun Times Football Helmet" />
+      <img src={helmetImg} alt="Super Wacky Fun Times Football Helmet" />
       <div className="header-text">
         <h1>Super Wacky Fun Times Fantasy Football League</h1>
         <p>The home for the Super Wacky Fun Times Fantasy Football League! See the managers, keepers, and records for the league. Located on <a href="https://sleeper.com/leagues/1182011896999837696/league" target="_blank" rel="noopener noreferrer">Sleeper</a></p>
