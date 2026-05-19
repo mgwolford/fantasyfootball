6ee246a37f9fcedf5d3869d02a28e999446b51c7
Matthew Wolford <mg.wolford@outlook.com>
2026-05-19 12:53:01 -0400

    Update repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // GitHub Pages project site: https://<user>.github.io/<repo>/
-  base: "/fantasyfootball/",
+  base: "/swftleague/",
   plugins: [react()],
 });
